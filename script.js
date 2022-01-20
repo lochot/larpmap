e736e6b70fff07eecb19a9f23c639646b9b999c7
lochot <lochot@free.fr>
2022-01-20 13:15:41 +0100

    decouverte scripts

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-WA.chat.sendChatMessage('Bienvenue sur le panneau !', 'Informations');
+WA.chat.sendChatMessage('Bienvenue sur le marché larp-place.com !', 'Informations');
+WA.chat.sendChatMessage('promenez vous et n\'hésitez pas à discuter avec les artisans', 'Informations');
