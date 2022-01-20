3542c175d3ddb87be1beb3012ee3ba91d1bcfe3d
lochot <lochot@free.fr>
2022-01-20 12:16:34 +0000

    Deploying to gh-pages from master @ e736e6b70fff07eecb19a9f23c639646b9b999c7 🚀

---
+++ myscript.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', () => {
+    WA.onInit().then(() => {
+        WA.chat.sendChatMessage('Bonjour '+WA.player.name, 'Informations');
+        WA.chat.sendChatMessage('Bienvenue sur le marché https://larp-place.com !', 'Informations');
+        WA.chat.sendChatMessage('promenez vous et n\'hésitez pas à discuter avec les artisans', 'Informations');
+        WA.chat.sendChatMessage('Si vous avez besoin d\'aide, tapez !aide', 'Informations');
+
+        WA.chat.onChatMessage(parseChat);
+
+        function parseChat(message) {
+            //if (!crewAwaitAnswer) return;
+                var l_msg = message.toLowerCase();
+            if (l_msg.includes('!aide')) {
+                WA.chat.sendChatMessage('Eh oui, on peux programmer des robots dans le chat !', 'Aide');
+            }
+        }
+
+
+
+        let currentPopup;
+            //const today = new Date();
+            //const time = today.getHours() + ":" + today.getMinutes();
+
+            WA.room.onEnterLayer('panneauZone').subscribe(() => {
+                console.log('toto');
+                currentPopup =  WA.ui.openPopup("textPanneauZone","Vous lisez un panneau", [{
+                    label: "Fermer",
+                    className: "primary",
+                    callback: (popup) => {
+                        // Close the popup when the "Close" button is pressed.
+                        popup.close();
+                    }
+                }]);
+            });
+
+            WA.room.onLeaveLayer('panneauZone').subscribe(closePopUp);
+
+            function closePopUp(){
+                if (currentPopup !== undefined) {
+                    currentPopup.close();
+                    currentPopup = undefined;
+                }
+            }
+    });
+});
