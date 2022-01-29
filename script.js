8254ead075b738a6f7dc93c79f656b51fecd727f
lochot <lochot@free.fr>
2022-01-29 14:22:56 +0100

    ajout des panneaux dans le module presta larp place

--- script.js
+++
@@ -1,2 +0,0 @@
-WA.chat.sendChatMessage('Bienvenue sur le marché larp-place.com !', 'Informations');
-WA.chat.sendChatMessage('promenez vous et n\'hésitez pas à discuter avec les artisans', 'Informations');
